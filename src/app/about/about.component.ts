4c1b2c07009483fe62708e236bfc7d4a103f927e
Burneo Espin Danny Jesus <djburneo@espol.edu.ec>
2023-08-04 12:14:26 -0500

    2.0

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+import { Anime } from '../interfaces/root';
+import { ServiceService } from '../providers/service.service';
+
+import DataTable from 'datatables.net-dt';
+import 'datatables.net-bs5';
+
+//import * as $ from 'jquery';
+//import 'bootstrap-table'; // Importa BootstrapTable
+//import 'bootstrap-table/dist/extensions/export/bootstrap-table-export'; // Importa la extensión de exportación
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css'],
+})
+export class AboutComponent {
+  public data: Anime[] = [];
+  public dataWithIndex: Anime[] = [];
+
+  constructor(private dataProvider: ServiceService) {}
+
+  ngOnInit() {
+    this.dataProvider.getResponse().subscribe((response) => {
+      this.data = response as Anime[];
+      this.dataWithIndex = this.data.map((anime, index) => ({ ...anime, index: index + 1 }));
+      this.initializeTable(this.dataWithIndex);
+      console.log(this.dataWithIndex);
+    });
+  }
+
+  private initializeTable(data: Anime[]) {
+    const columns = [
+      { data: 'index', title: 'Índice' },
+      { data: 'anime' },
+      { data: 'rate' },
+      { data: 'votes' },
+    ];
+    new DataTable('#tabla', {
+      data: data,
+      columns: columns,
+      pageLength: 10,
+      language: {
+        lengthMenu: 'Mostrar _MENU_ registros por página',
+        info: 'Mostrando de _START_ a _END_ de un total de _TOTAL_ registros',
+        search: 'Buscar:',
+        paginate: {
+          first: 'Primero',
+          last: 'Último',
+          next: 'Siguiente',
+          previous: 'Anterior',
+        },
+      },
+    });
+  }
+
+  // Boostrap-table
+  // private initializeTable(data: Anime[]) {
+  //   const columns = [
+  //     { field: 'anime', title: 'Nombre' },
+  //     { field: 'rate', title: 'Puntuacion' },
+  //     { field: 'votes', title: 'Votos' },
+  //   ];
+  //   // Inicializar la tabla cuando se obtienen los datos
+  //   const $table = $('#table');
+  //   $table.bootstrapTable({
+  //     data: data,
+  //     columns: columns,
+  //     search: true,
+  //     pagination: true,
+  //   });
+
+  //   $table.bootstrapTable('load', data);
+  // }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MastheadComponent } from './masthead/masthead.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SignupComponent } from './signup/signup.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+import { HttpClientModule } from '@angular/common/http'; // Importa HttpClientModule
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MastheadComponent,
+    AboutComponent,
+    ProjectsComponent,
+    SignupComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
